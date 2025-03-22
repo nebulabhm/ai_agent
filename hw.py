0bc9820cdafa5b69f03b578c95200dc58e2b118e
nebulabhm <nebulabhm@gmail.com>
2025-03-22 23:44:58 +0800

    refactor(hw.py): 重构代码以使用RunnableSequence并添加起名大师功能
    重构代码以使用`RunnableSequence`替代`LLMChain`，提升代码的可读性和维护性。新增起名大师功能，支持生成特定国家的名字示例。同时，添加了文件头注释和部分方法的文档字符串，以提高代码的可维护性。

--- hw.py
+++ hw.py
@@ -1,9 +1,17 @@
+'''
+Author: nebulabhm nebulabhm@gmail.com
+Date: 2025-03-22 21:50:57
+LastEditors: nebulabhm nebulabhm@gmail.com
+LastEditTime: 2025-03-22 23:28:03
+FilePath: /ai_agent/hw.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import os
 from typing import Any, Dict, List, Mapping, Optional
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.llms.base import LLM
-from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableSequence
 from deepseek_ai import DeepSeekAI
 
 # 设置API密钥
@@ -23,6 +31,12 @@ class DeepSeekLLM(LLM):
     
     @property
     def _llm_type(self) -> str:
+        """
+        The type identifier for the DeepSeek language model.
+        
+        Returns:
+            str: A string literal "deepseek" representing the LLM type.
+        """
         return "deepseek"
     
     def _call(
@@ -32,6 +46,7 @@ class DeepSeekLLM(LLM):
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> str:
+
         messages = [{"role": "user", "content": prompt}]
         # 移除stop参数，DeepSeek API不支持该参数
         response = self._client.chat.completions.create(
@@ -47,16 +62,33 @@ class DeepSeekLLM(LLM):
 llm = DeepSeekLLM()
 
 # 创建提示模板
-prompt = PromptTemplate(
+prompt_template = PromptTemplate(
     input_variables=["query"],
     template="{query}"
 )
 
-# 创建LLMChain
-chain = LLMChain(llm=llm, prompt=prompt)
+# 创建RunnableSequence
+chain = prompt_template | llm
 
-# 使用LangChain调用DeepSeek API并获取响应
-response = chain.run("Hello world!")
+# 创建起名大师的提示模板
+name_prompt = PromptTemplate.from_template("你是一个起名大师，请模仿示例起3个{country}名字，比如男孩经常被叫做{boy}，女孩经常被叫做{girl}")
 
-# 打印响应结果
+# 创建起名大师的RunnableSequence
+name_chain = name_prompt | llm
+
+# 使用第一个RunnableSequence调用DeepSeek API并获取响应
+response = chain.invoke({"query": "介绍一下你自己"})
+print("自我介绍：")
 print(response)
+
+# 使用format方法格式化提示模板，然后将格式化后的字符串传递给LLM
+formatted_prompt = name_prompt.format(country="中国特色的", boy="狗蛋", girl="翠花")
+print("使用format方法的提示：")
+print(formatted_prompt)
+response = llm.invoke(formatted_prompt)
+print("起名大师的回答：")
+print(response)
+
+
+
+
